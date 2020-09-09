7b7dc7b026663f34b57497366074d2ebca61af6b
CzRger <526948392@qq.com>
2020-09-09 10:31:32 +0800

    曹钊瑞

--- src/main/java/com/example/talent/sep/StreamAlerter.java
+++ src/main/java/com/example/talent/sep/StreamAlerter.java
@@ -1,15 +1,42 @@
 package com.example.talent.sep;
 
+import java.util.HashMap;
+
 public class StreamAlerter {
 //    private RingBuffer ring;
 //    private Trie trie;
+    private HashMap keysWords = new HashMap();
+    private int keyLength;
+    private int currentIndex;
+    private String currentStr;
+
 
     public StreamAlerter(String[] strings) {
-        // ...
+        for (String str:strings) {
+            keysWords.put(str.charAt(0), str);
+        }
     }
-
     public boolean query(char ch) {
-        // ...
+        if (currentStr == null && keysWords.get(ch) != null) {
+            currentStr = (String) keysWords.get(ch);
+            keyLength = currentStr.length();
+            currentIndex = 0;
+        }
+        if (currentStr == null) {
+            return false;
+        } else {
+            if (ch == currentStr.charAt(currentIndex)) {
+                if (currentIndex == keyLength - 1) {
+                    currentStr = null;
+                    return true;
+                } else {
+                    currentIndex++;
+                    return false;
+                }
+            } else {
+                currentStr = null;
+            }
+        }
         return false;
     }
 }
